81350317cc13019ef5f0ffa9d8246c861e5d6f12
rohan patel <rpatel5@bsd220.org>
2025-01-22 09:30:56 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Rohan Patel on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
